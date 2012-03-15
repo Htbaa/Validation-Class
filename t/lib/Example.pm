d69fce5abebf3ab8d28714a1021551bdd9e260c6
Al Newkirk <anewkirk@ana.io>
2012-03-15 01:57:55 -0400

    removed deleted files

--- t/lib/Example.pm
+++
@@ -1,7 +0,0 @@
-package Example;
-
-use Validation::Class;
-
-set { classes => 1 }; # load child classses
-
-1;
--- t/previous/00-load.t
+++
@@ -1,4 +0,0 @@
-use Test::More tests => 1;
-
-# load module
-BEGIN { use_ok( 'Validation::Class' ) }
